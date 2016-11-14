a23e61f46fe3a35e63a7acd264129478f64edd24
kasha.akrami <kasha.akrami@MU-C1-14.mullen.pvt.k12.co.us>
2016-11-14 15:03:28 -0700

    initial

---
+++ SIS project/src/Runner.java
@@ -0,0 +1,12 @@
+
+public class Runner
+	{
+
+		public static void main(String[] args)
+			{
+				// TODO Auto-generated method stub
+				//Kasha, Cooper, Marie
+				System.out.println();
+			}
+
+	}
